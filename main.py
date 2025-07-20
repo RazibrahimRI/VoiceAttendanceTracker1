b1604ee70e09a6daee83756cc01f66044be5d2d3
Razi Ibrahim <141431477+RazibrahimRI@users.noreply.github.com>
2025-07-20 20:42:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import routes  # Import routes to register them
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This reads the .env file
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SECRET_KEY'] = os.getenv("SESSION_SECRET")
